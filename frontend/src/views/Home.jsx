531d14e557f0a3d0b1404775c2c19b7cb60b7815
caxky <k_james7@outlook.com>
2024-01-29 23:36:25 -0500

    better homepage

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,75 +1,29 @@
 import React from 'react';
+import { Button, Container, CssBaseline, Grid } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Card, CardContent, Typography, Grid, Divider } from '@mui/material';
-import SentimentIcon from '@mui/icons-material/SentimentSatisfied';
-import ThariIcon from '@mui/icons-material/EmojiEmotions';
-import CollabIcon from '@mui/icons-material/Group';
 import './Home.scss';
-
-// Create a dark theme
-const darkTheme = createTheme({
-  palette: {
-    type: 'dark',
-    primary: {
-      main: '#424242', // Main color for primary elements
-    },
-    background: {
-      default: '#000000', // Background color
-      paper: '#303030', // Paper color (used for cards)
-    },
-  },
-});
 
 const Home = () => {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <div className='home'>
-        <Grid container spacing={3} justifyContent="center">
-          {/* Sentiment Section */}
-          <Grid item xs={12} md={4}>
-            <Card elevation={0}> {/* Set elevation to 0 to remove the shadow */}
-              <CardContent>
-                <SentimentIcon fontSize="large" />
-                <Typography variant="h5">Sentiment</Typography>
-                <Divider />
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
+    <div className="home">
+      <Container>
+        <CssBaseline />
+        {/* Your Logo Component or <img> tag */}
+        <img src="images/dark_quantop_large_logo.svg" alt="Quantop Logo" className="logo" />
 
-          {/* Thari Section */}
-          <Grid item xs={12} md={4}>
-            <Card elevation={0}>
-              <CardContent>
-                <ThariIcon fontSize="large" />
-                <Typography variant="h5">Thari</Typography>
-                <Divider />
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-
-          {/* Collab Section */}
-          <Grid item xs={12} md={4}>
-            <Card elevation={0}>
-              <CardContent>
-                <CollabIcon fontSize="large" />
-                <Typography variant="h5">Collab</Typography>
-                <Divider />
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </Typography>
-              </CardContent>
-            </Card>
+        {/* Optional: Add margin or spacing */}
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item>
+            <Button variant="contained" color="primary" className='grayscale-button'>
+              ENTER APP
+            </Button>
           </Grid>
         </Grid>
-      </div>
-    </ThemeProvider>
+
+        {/* Enter App Button */}
+        
+      </Container>
+    </div>
   );
 };
 
